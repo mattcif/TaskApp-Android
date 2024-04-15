d107d8fc09d5fc4cf9b498c5e0a41d0cc7ad103d
mattcif <matthewoaklucky@gmail.com>
2024-04-15 20:48:48 -0300

    Criando Config TabLayout

--- app/src/main/java/com/example/taskapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/auth/LoginFragment.kt
@@ -29,6 +29,11 @@ class LoginFragment : Fragment() {
     }
 
     private fun initListener() {
+
+        binding.btnLogin.setOnClickListener{
+            findNavController().navigate(R.id.action_global_homeFragment)
+        }
+
         binding.btnRegister.setOnClickListener{
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
