fb9884317c863e9e47c5c2a612a400face266cd1
mattcif <matthewoaklucky@gmail.com>
2024-04-15 12:09:18 -0300

    criando layout tela de registro

--- app/src/main/java/com/example/taskapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/auth/RegisterFragment.kt
@@ -7,19 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentLoginBinding
+import com.example.taskapp.databinding.FragmentRegisterBinding
+import com.example.taskapp.databinding.FragmentSplashBinding
 
 
-class LoginFragment : Fragment() {
-    private var _binding: FragmentLoginBinding? = null
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
