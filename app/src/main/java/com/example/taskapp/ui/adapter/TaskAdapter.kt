de0271728cd2ca2956b6a348d2e59ca50836a10c
mattcif <matthewoaklucky@gmail.com>
2024-04-18 20:30:48 -0300

    configurando indicadores das tasks

--- app/src/main/java/com/example/taskapp/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/taskapp/ui/adapter/TaskAdapter.kt
@@ -1,33 +1,74 @@
 package com.example.taskapp.ui.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.example.taskapp.R
+import com.example.taskapp.data.model.Status
 import com.example.taskapp.data.model.Task
 import com.example.taskapp.databinding.ItemTaskBinding
 
 class TaskAdapter(
+    private val context: Context,
     private val taskList: List<Task>
 ) : RecyclerView.Adapter<TaskAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(ItemTaskBinding.inflate(LayoutInflater.from(parent.context),
-            parent,
-            false))
+        return MyViewHolder(
+            ItemTaskBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
-
 
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val task = taskList[position]
 
         holder.binding.textDescription.text = task.description
+
+        setIndicators(task, holder)
+    }
+
+    private fun setIndicators(task: Task, holder: MyViewHolder) {
+        when (task.status) {
+            Status.TODO -> {
+                holder.binding.btnBack.isVisible = false
+            }
+
+            Status.DOING -> {
+                holder.binding.btnBack.setColorFilter(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.color_status_todo
+                    )
+                )
+
+                holder.binding.btnNext.setColorFilter(
+                    ContextCompat.getColor(
+                        context, R.color.color_status_done
+                    )
+                )
+            }
+
+            Status.DONE -> {
+                holder.binding.btnNext.isVisible = false
+            }
+
+            else -> {}
+
+        }
+
     }
 
     override fun getItemCount() = taskList.size
 
-    inner class MyViewHolder(val binding: ItemTaskBinding)
-        : RecyclerView.ViewHolder(binding.root) {
+    inner class MyViewHolder(val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root) {
 
     }
 }
