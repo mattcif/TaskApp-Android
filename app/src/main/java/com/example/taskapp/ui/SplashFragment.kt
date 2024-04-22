ae452dc05deaaea3aa7b7b19e30e6baef96f4985
mattcif <matthewoaklucky@gmail.com>
2024-04-22 03:33:48 -0300

    verificando prelog

--- app/src/main/java/com/example/taskapp/ui/SplashFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/SplashFragment.kt
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentSplashBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.util.logging.Handler
 
 
@@ -16,6 +19,8 @@ class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,11 +34,21 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = Firebase.auth
+
+
         android.os.Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 2000)
     }
 
     private fun checkAuth() {
-        findNavController().navigate(R.id.action_splashFragment_to_authentication)
+        val currentUser = auth.currentUser
+        if(currentUser != null) {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        } else {
+            findNavController().navigate(R.id.action_splashFragment_to_authentication)
+
+        }
+
     }
 
     override fun onDestroyView() {
