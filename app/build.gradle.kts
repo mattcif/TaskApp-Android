5f859290e5a2e5a19eddda4d881b9455c479d415
mattcif <matthewoaklucky@gmail.com>
2024-04-22 03:25:36 -0300

    salvando user no auth firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("kotlin-parcelize")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -51,6 +52,11 @@ dependencies {
     // Navigation
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
+    // FireBase
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+
+    implementation("com.google.firebase:firebase-auth")
 
     // Test
     testImplementation(libs.junit)
--- app/src/main/java/com/example/taskapp/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/auth/RegisterFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentLoginBinding
@@ -13,12 +14,19 @@ import com.example.taskapp.databinding.FragmentRegisterBinding
 import com.example.taskapp.databinding.FragmentSplashBinding
 import com.example.taskapp.util.initToolbar
 import com.example.taskapp.util.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,12 +40,14 @@ class RegisterFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initToolbar(binding.toolbar)
 
+        auth = Firebase.auth
+
         initListener()
     }
 
     private fun initListener() {
 
-        binding.btnRegister.setOnClickListener{
+        binding.btnRegister.setOnClickListener {
             validateData()
         }
 
@@ -47,15 +57,36 @@ class RegisterFragment : Fragment() {
         val email = binding.edtEmail.text.toString().trim()
         val password = binding.edtPassword.text.toString().trim()
 
-        if(email.isNotEmpty()){
-            if(password.isNotEmpty()) {
-                Toast.makeText(requireContext(), "Tudo Certo", Toast.LENGTH_SHORT).show()
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
+
+                binding.progressBar.isVisible = true
+
+                registerUser(email, password)
+
+
             } else {
                 showBottomSheet(message = getString(R.string.password_empty_register_fragment))
             }
         } else {
             showBottomSheet(message = getString(R.string.email_empty_register_fragment))
         }
+    }
+
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_global_homeFragment)
+                } else {
+                    binding.progressBar.isVisible = true
+
+
+                    Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
     }
 
 
