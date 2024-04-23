67a11f5749a34f282b3e974155cb4f2231692dbe
mattcif <matthewoaklucky@gmail.com>
2024-04-23 11:27:01 -0300

    login no app

--- app/src/main/java/com/example/taskapp/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/auth/LoginFragment.kt
@@ -6,15 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
 import com.example.taskapp.databinding.FragmentLoginBinding
 import com.example.taskapp.util.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +33,8 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
 
         initListener()
     }
@@ -52,7 +61,10 @@ class LoginFragment : Fragment() {
         if(email.isNotEmpty()){
             if(password.isNotEmpty()) {
 
-                findNavController().navigate(R.id.action_global_homeFragment)
+                binding.progressBar.isVisible = true
+
+                loginUser(email, password)
+
 
             } else {
                 showBottomSheet(message = getString(R.string.password_empty))
@@ -60,6 +72,19 @@ class LoginFragment : Fragment() {
         } else {
             showBottomSheet(message = getString(R.string.email_empty))
         }
+    }
+
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener() { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_global_homeFragment)
+                } else {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     override fun onDestroyView() {
