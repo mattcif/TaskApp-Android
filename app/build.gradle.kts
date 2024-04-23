d550a4d814ce951d68338b4e55748bd007740b41
mattcif <matthewoaklucky@gmail.com>
2024-04-23 12:11:39 -0300

    salvando tarefas no realtime db

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
 
     implementation("com.google.firebase:firebase-auth")
-
+    implementation("com.google.firebase:firebase-database")
     // Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/taskapp/ui/FormTaskFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/FormTaskFragment.kt
@@ -5,17 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.taskapp.R
+import com.example.taskapp.data.model.Status
+import com.example.taskapp.data.model.Task
 import com.example.taskapp.databinding.FragmentFormTaskBinding
 import com.example.taskapp.util.initToolbar
 import com.example.taskapp.util.showBottomSheet
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class FormTaskFragment : Fragment() {
 
     private var _binding: FragmentFormTaskBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var task: Task
+    private var status: Status = Status.TODO
+    private var newTask: Boolean = true
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var reference: DatabaseReference
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,27 +47,72 @@ class FormTaskFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initToolbar(binding.toolbar)
 
+
+        reference = Firebase.database.reference
+        auth = Firebase.auth
+
         initListener()
     }
 
 
     private fun initListener() {
 
-        binding.btnSave.setOnClickListener{
+        binding.btnSave.setOnClickListener {
             validateData()
         }
 
+        binding.rgStatus.setOnCheckedChangeListener { _, checkedId ->
+            status = when (id) {
+                R.id.rbTodo -> Status.TODO
+                R.id.rbDoing -> Status.DOING
+                else -> Status.DONE
+            }
+        }
 
     }
 
     private fun validateData() {
         val description = binding.edtDescription.text.toString().trim()
 
-        if(description.isNotEmpty()){
-            Toast.makeText(requireContext(), "Tudo Certo", Toast.LENGTH_SHORT).show()
+        if (description.isNotEmpty()) {
+
+            binding.progressBar.isVisible = true
+
+            if (newTask) task = Task()
+            task.id = reference.database.reference.push().key ?: ""
+            task.description = description
+            task.status = status
+
+            saveTask()
         } else {
             showBottomSheet(message = getString(R.string.description_empty_form_task_fragment))
         }
+    }
+
+    private fun saveTask() {
+        reference
+            .child("tasks")
+            .child(auth.currentUser?.uid ?: "")
+            .child(task.id)
+            .setValue(task).addOnCompleteListener { result ->
+                if (result.isSuccessful) {
+                    Toast.makeText(
+                        requireContext(),
+                        R.string.text_save_success_form_task_fragment,
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    if (newTask) {
+                        findNavController().popBackStack()
+                    } else { // editando tarefa
+                        binding.progressBar.isVisible = false
+
+                    }
+                } else {
+                    binding.progressBar.isVisible = false
+                    showBottomSheet(message = getString(R.string.error_generic))
+                }
+            }
     }
 
     override fun onDestroy() {
